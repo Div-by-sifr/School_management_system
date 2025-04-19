00b2803097c858a07c249c84483b4b0d049ebc18
SALEH ALGABRI <s96557867@gmail.com>
2025-04-19 18:34:26 +0300

    Add URLs to go to the home page with unfollowing and deleting some files

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 app_name = 'accounts'
 urlpatterns = [
+    path('', views.home,name=''),
     path('home/', views.home,name='home'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'), 
