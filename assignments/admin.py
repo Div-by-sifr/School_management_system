4086ef057da5c3d8784b361afb1c1a94923c12e4
SALEH ALGABRI <s96557867@gmail.com>
2025-04-29 00:55:04 +0300

    The modules were removed from the assignments app and created in the grades app.

--- assignments/admin.py
+++ assignments/admin.py
@@ -1,11 +1,3 @@
 from django.contrib import admin
-from .models import Month,MonthlyGrade,TermGrade
+
 # Register your models here.
-
-class MonthAdmin(admin.ModelAdmin):
-    list_display=['id','month_name','month_order']
-    
-
-admin.site.register(Month,MonthAdmin)
-admin.site.register(MonthlyGrade)
-admin.site.register(TermGrade)
